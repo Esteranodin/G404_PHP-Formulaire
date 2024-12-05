3ca887d0676f1adb9c647ea45919802153ed72f1
esteranodin <esteranodin@hotmail.fr>
2024-12-05 14:17:27 +0100

    SETUP

---
+++ Code_fourni_G404/compte_creer.php
@@ -0,0 +1,27 @@
+// compte_creer.php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>votre à été crée</h1>
+
+    <?php
+
+    if (isset($_GET['lastName'], $_GET['firstName'])) {
+        echo "<p>bienvenue {$_GET['firstName']}  {$_GET['lastName']} </p>";
+    }
+
+
+    ?>
+
+
+
+</body>
+
+</html>
---
+++ Code_fourni_G404/index.php
@@ -0,0 +1,59 @@
+<?php
+// index.php
+
+if (isset($_GET['error'])) {
+    $error = $_GET['error'];
+} else {
+    $error = false;
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Traitement de formulaire</title>
+</head>
+
+<body>
+    <h1>hello world</h1>
+
+
+    <?php
+    if ($error == 1) {
+        echo '<p> Je te vois supprimer mes champs ...</p>';
+    }
+
+    if ($error == 2) {
+        echo '<p> Il faut remplir tous les champs !!</p>';
+    }
+
+    ?>
+
+    <form action="./process/default_form_process.php" method="post">
+        <label for="firstName"> Prenom :</label>
+        <input type="text" name="firstName" id="firstName" required maxlength="10">
+
+        <label for="lastName"> Nom de famille :</label>
+        <input type="text" name="lastName" id="lastName">
+
+        <label for="age"> Age :</label>
+        <input type="text" name="age" id="age">
+
+        <label for="email"> Email :</label>
+        <input type="email" name="email" id="email">
+
+        <label for="password"> Password :</label>
+        <input type="password" name="password" id="password">
+
+        <input type="submit" value="Envoyer">
+
+    </form>
+
+</body>
+
+</html>
